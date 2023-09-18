41661f0a62c6b0fa3d3950ac8d3558349b4cf6ea
Toni Alvarez <aalvarez@enerclic.es>
2023-09-18 12:57:56 +0200

    r

--- src/components/GraficaGastosMaxMin/GraficaGastosMaxMin.jsx
+++ src/components/GraficaGastosMaxMin/GraficaGastosMaxMin.jsx
@@ -19,11 +19,17 @@ const GraficaGastos = ({ data }) => {
   accessibility(Highcharts);
   const totalGastos = data
     .filter((item) => item.tipo === "Gasto")
-    .reduce((total, item) => total + parseFloat(item.cantidad).toFixed(2), 0);
+    .reduce(
+      (total, item) => total + Number(parseFloat(item.cantidad).toFixed(2)),
+      0
+    );
 
   const totalIngresos = data
     .filter((item) => item.tipo === "Ingreso")
-    .reduce((total, item) => total + parseFloat(item.cantidad).toFixed(2), 0);
+    .reduce(
+      (total, item) => total + Number(parseFloat(item.cantidad).toFixed(2)),
+      0
+    );
 
   const diferencia = totalIngresos - totalGastos;
   const diferenciaLabel = diferencia >= 0 ? "Ganancias" : "Pérdidas";
